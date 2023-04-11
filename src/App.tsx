4b8cc9d9d022671cc6d308e69c71aa9f874fa3b4
Daniel Villa <4193348+kingnebby@users.noreply.github.com>
2023-04-11 13:42:59 -0700

    chore: add lines test

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,22 @@ import OrbitCtrls from './components/OrbitCtrls';
 import Forest from './views/Forest';
 import MergingManyPerfTest from './views/MergingManyPerfTest';
 import ShowLod from './views/ShowLod';
+import StressLines from './views/StressLines';
 
 function App() {
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
-      <Canvas style={{ background: 'black' }} camera={{ position: [3, 3, 3], fov: 50, far: 2000 }}
+      <Canvas style={{ background: 'black' }} camera={{ position: [30, 30, 30], fov: 50, far: 2000 }}
         shadows={true}
       >
-        <Stats />
+        <Stats className='stats' />
         <Background />
         <OrbitCtrls />
         <axesHelper args={[5]} />
         <ambientLight intensity={0.2} />
         {/* <Forest /> */}
-        <ShowLod />
+        {/* <ShowLod /> */}
+        <StressLines />
       </Canvas>
     </div>
   );
---
+++ src/views/StressLines.tsx
@@ -0,0 +1,50 @@
+import { Line } from '@react-three/drei'
+import { Vector3 } from 'three'
+
+function StressLines() {
+
+  function createLine(id: number, vectorCount: number) {
+    const vectors = generateZigZagVectors(id, vectorCount)
+    // const vectors = generateRandomVectors(id, vectorCount)
+    const color = '#' + Math.floor(Math.random() * 16777215).toString(16)
+    return (
+      <mesh key={id}>
+        <Line key={id} points={vectors} color={color} />
+      </mesh>
+    )
+  }
+
+  const meshNumber = 300
+  const list = new Array(meshNumber).fill(0)
+
+  return (
+    <>
+      {/* <mesh> */}
+      {list.map((_, index) => { return createLine(index, 300) })}
+      {/* </mesh> */}
+    </>
+  )
+}
+
+// draw a big square of rectangles
+function generateZigZagVectors(offset: number, count: number): Vector3[] {
+  // const length = count
+  const vectors: Vector3[] = []
+  for (let i = 0; i < count; i++) {
+    const mod = i % 2 === 0 ? 1 : -1
+    vectors.push(new Vector3(offset + mod, i, 0))
+  }
+  return vectors
+}
+
+
+function generateRandomVectors(offset: number, count: number): Vector3[] {
+  const vectors: Vector3[] = []
+  for (let i = 0; i < count; i++) {
+    vectors.push(new Vector3(Math.random() * 10, Math.random() * 10, Math.random() * 10))
+  }
+  return vectors
+
+}
+
+export default StressLines
