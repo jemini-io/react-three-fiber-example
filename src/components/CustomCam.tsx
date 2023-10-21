bd04faa7182e0289cd5e3a66812b187491cdc0b8
Daniel Villa <4193348+kingnebby@users.noreply.github.com>
2023-10-21 08:31:49 -0700

    feat: got orthocam working

--- src/components/CustomCam.tsx
+++ src/components/CustomCam.tsx
@@ -2,15 +2,17 @@ import { OrthographicCamera, PerspectiveCamera } from "@react-three/drei"
 import { useFrame, useThree } from "@react-three/fiber"
 import { WheelEvent, useEffect, useState } from "react"
 import { useEvent } from "react-use"
-import { Vector3 } from "three"
+import { Box3, Mesh, Object3D, Vector3 } from "three"
 import { OrthographicCamera as OrthographicCameraType } from "three"
-import { OrbitControls } from "three-stdlib"
+import { Geometry, OrbitControls } from "three-stdlib"
 
 export const CustomCam = () => {
   const camera = useThree(({ camera }) => camera)
   const controls = useThree(({ controls }) => controls)
   const gl = useThree(({ gl }) => gl)
+  const scene = useThree(({ scene }) => scene)
   const [scrollDelta, setScrollDelta] = useState(0)
+  const [maxDim, setMaxDim] = useState(10)
 
   const noEvents = true
 
@@ -26,13 +28,47 @@ export const CustomCam = () => {
     oCam.top = newVewPort
     oCam.bottom = -newVewPort
   }
+
+  /**
+   * This will set the max distance of the camera/viewport
+   * based on the bounding box of the scene.
+   * Could also be used as the `far` value of the camera.
+   */
+  const sceneBB = new Box3();
+  const localBbBox = new Box3();
+  const dims = new Vector3()
+  const computeSceneBoundingBox = () => {
+    scene.traverse((child) => {
+      if ((child as Mesh).geometry) {
+        const { geometry } = child as Mesh
+        geometry.computeBoundingBox();
+        localBbBox.copy(geometry.boundingBox as Box3);
+        child.updateMatrixWorld(true);
+        localBbBox.applyMatrix4(child.matrixWorld);
+
+        sceneBB.union(localBbBox);
+      }
+    })
+    sceneBB.getSize(dims)
+    setMaxDim(Math.max(...dims.toArray()))
+  }
+
+  // I'd like to run these once when the scene loads but this approach
+  // isn't working. It's just here for reference.
   useEffect(() => {
     resizeViewport(gl.domElement.width, gl.domElement.height)
+    // arbitrary hook to update the maxDimension of the scene.
+    computeSceneBoundingBox()
   }, [])
+
+  /**
+   * This will resize the viewport when the window is resized.
+   */
   useEvent('resize', () => {
     resizeViewport(gl.domElement.width, gl.domElement.height)
+    // arbitrary hook to update the maxDimension of the scene.
+    computeSceneBoundingBox()
   })
-
 
   // Custom zoom handler
   useEvent('wheel', (event: WheelEvent) => {
@@ -49,37 +85,38 @@ export const CustomCam = () => {
       const direction = Math.sign(scrollDelta)
 
       // Set reasonable limits, otherwise we end up in the negative
-      // TODO: use the math way to divide distance to object so that you're
-      // zooming into the target.
       const distanceToTarget = oCtrls.target.distanceTo(camera.position)
-      if (distanceToTarget < 5 && direction < 0) {
+      // Set max custom zoom
+      if (distanceToTarget < 5) {
         // Can zoom in more once you hit the max camera position
-        camera.zoom *= 1.1
-        return;
+        if (direction < 0) {
+          camera.zoom *= 1.1
+          return;
+        }
+        if (direction > 0 && camera.zoom > 1) {
+          camera.zoom = 1
+          return;
+        }
       }
-      if (distanceToTarget > 50 && direction > 0) {
-        camera.zoom /= 1.1
+      // Set min custom zoom
+      if (distanceToTarget > maxDim && direction > 0) {
         return
       }
-      camera.zoom = 1
 
-      // TODO: play with moveDistance and camera.position
-      // right now it's 10% change of the top value and
-      // +/- 1 unit in the direction of the camera.
-      // TODO: it seems like we should move both these values linearly
-      const viewPortResizePercent = 0.1;
-      const cameraMoveUnit = direction * 1;
+      // Set a viewport zoom factor
+      const viewPortResizePercent = 0.2;
       const moveDistance = oCam.top * (direction * viewPortResizePercent)
       resizeViewport(gl.domElement.width, gl.domElement.height, moveDistance)
 
       // Move the camera's position without changing the zoom
       // so that the LOD calculations are correct
-      const dirNormal = camera.getWorldDirection(new Vector3()).normalize()
-      camera.position.sub(dirNormal.multiplyScalar(cameraMoveUnit))
-
-      //
-      console.log('oCam.top', oCam.top)
-      console.log('position', camera.position.toArray())
+      // Get the opposite direction of the camera
+      // set the position is from the target, in that direction
+      // based on the viewport size
+      const flippedDirNormal = camera.getWorldDirection(new Vector3()).normalize().multiplyScalar(-1)
+      const distanceFactor = oCam.top * oCam.right;
+      const newPosition = oCtrls.target.clone().add(flippedDirNormal.multiplyScalar(distanceFactor))
+      camera.position.copy(newPosition)
     }
 
 
